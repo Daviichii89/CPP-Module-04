a58ca06738b7514ec68492f6473dc3d8188cb362
David Fernandez <davichi89@gmail.com>
2025-04-24 21:47:23 +0200

    init ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,46 @@
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+
+int main()
+{
+    Animal* animals[4];
+    
+    animals[0] = new Dog();
+    animals[1] = new Cat();
+    animals[2] = new Dog();
+    animals[3] = new Cat();
+
+    std::cout << "--------------------------------------" << std::endl;
+
+    Animal* dogCopy = new Dog(*(dynamic_cast<Dog*>(animals[0])));
+    Animal* catCopy = new Cat(*(dynamic_cast<Cat*>(animals[1])));
+
+    std::cout << "--------------------------------------" << std::endl;
+
+    for (int i = 0; i < 4; ++i) {
+        std::cout << animals[i]->getType() << " makes: ";
+        animals[i]->makeSound();
+    }
+
+    std::cout << "--------------------------------------" << std::endl;
+
+    std::cout << "dogCopy type: " << dogCopy->getType() << " makes: ";
+    dogCopy->makeSound();
+
+    std::cout << "catCopy type: " << catCopy->getType() << " makes: ";
+    catCopy->makeSound();
+
+    std::cout << "-------------------------------------------------" << std::endl;
+    
+    for (int i = 0; i < 4; ++i) {
+        delete animals[i];
+    }
+
+    delete dogCopy;
+    delete catCopy;
+
+    std::cout << "-------------------------------------------------" << std::endl;
+
+    return 0;
+}
