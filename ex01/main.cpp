c9b37e84963288942b9c0ee16f54704fa2045be3
David Fernandez <davichi89@gmail.com>
2025-04-24 21:46:18 +0200

    finish ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,31 +1,46 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
-#include "WrongCat.hpp"
 
 
 int main()
 {
-    const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound();
-    j->makeSound();
-    meta->makeSound();
-
-    delete meta;
-    delete j;
-    delete i;
-    std::cout << "------------------------" << std::endl;
-    const WrongAnimal* wrongMeta = new WrongAnimal();
-    const WrongAnimal* wrongCat = new WrongCat();
-    std::cout << wrongCat->getType() << " " << std::endl;
-    wrongCat->makeSound();
-    wrongMeta->makeSound();
+    Animal* animals[4];
     
-    delete wrongMeta;
-    delete wrongCat;
+    animals[0] = new Dog();
+    animals[1] = new Cat();
+    animals[2] = new Dog();
+    animals[3] = new Cat();
+
+    std::cout << "--------------------------------------" << std::endl;
+
+    Animal* dogCopy = new Dog(*(dynamic_cast<Dog*>(animals[0])));
+    Animal* catCopy = new Cat(*(dynamic_cast<Cat*>(animals[1])));
+
+    std::cout << "--------------------------------------" << std::endl;
+
+    for (int i = 0; i < 4; ++i) {
+        std::cout << animals[i]->getType() << " makes: ";
+        animals[i]->makeSound();
+    }
+
+    std::cout << "--------------------------------------" << std::endl;
+
+    std::cout << "dogCopy type: " << dogCopy->getType() << " makes: ";
+    dogCopy->makeSound();
+
+    std::cout << "catCopy type: " << catCopy->getType() << " makes: ";
+    catCopy->makeSound();
+
+    std::cout << "-------------------------------------------------" << std::endl;
+    
+    for (int i = 0; i < 4; ++i) {
+        delete animals[i];
+    }
+
+    delete dogCopy;
+    delete catCopy;
+
+    std::cout << "-------------------------------------------------" << std::endl;
 
     return 0;
 }
